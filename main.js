da1f0bd190557b6b0d13f57a58fae5c8d95d72c3
Adrien Joly <adrien@whyd.com>
2017-01-07 08:39:52 +0100

    first working version

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -8,17 +6,19 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 1024,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: false, // to let jquery load in web mode
+    },
+  })
 
   // and load the index.html of the app.
-  win.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+  win.loadURL('https://openwhyd.org/')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -50,6 +50,3 @@ app.on('activate', () => {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
