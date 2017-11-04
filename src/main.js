280d8cbd601abceec08aa097fb53fbebc8b7c37e
Adrien Joly <adrien.joly@gmail.com>
2017-11-04 15:19:28 +0100

    refactoring: menu in its own file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-const {app, shell, BrowserWindow, Menu} = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
+const { menu } = require('./menu')
 
 require('electron-debug')({
   enabled: true, // => DevTools are also usable in production
@@ -8,91 +9,6 @@ require('electron-debug')({
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
-
-
-const template = [
-  {
-    label: 'Edit',
-    submenu: [
-      {role: 'undo'},
-      {role: 'redo'},
-      {type: 'separator'},
-      {role: 'cut'},
-      {role: 'copy'},
-      {role: 'paste'},
-      {role: 'pasteandmatchstyle'},
-      {role: 'delete'},
-      {role: 'selectall'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {role: 'reload'},
-      {role: 'toggledevtools'},
-      {type: 'separator'},
-      {role: 'resetzoom'},
-      {role: 'zoomin'},
-      {role: 'zoomout'},
-      {type: 'separator'},
-      {role: 'togglefullscreen'}
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {role: 'minimize'},
-      {role: 'close'}
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click () { shell.openExternal('https://github.com/openwhyd/openwhyd-electron') }
-      }
-    ]
-  }
-]
-
-if (process.platform === 'darwin') { // required by MacOS
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {role: 'about'},
-      {type: 'separator'},
-      {role: 'services', submenu: []},
-      {type: 'separator'},
-      {role: 'hide'},
-      {role: 'hideothers'},
-      {role: 'unhide'},
-      {type: 'separator'},
-      {role: 'quit'}
-    ]
-  })
-
-  // Edit menu
-  template[1].submenu.push(
-    {type: 'separator'},
-    {
-      label: 'Speech',
-      submenu: [
-        {role: 'startspeaking'},
-        {role: 'stopspeaking'}
-      ]
-    }
-  )
-
-  // Window menu
-  template[3].submenu = [
-    {role: 'close'},
-    {role: 'minimize'},
-    {role: 'zoom'},
-    {type: 'separator'},
-    {role: 'front'}
-  ]
-}
 
 function createWindow () {
   // Create the browser window.
@@ -105,8 +21,7 @@ function createWindow () {
     },
   })
 
-  const menu = Menu.buildFromTemplate(template)
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+  Menu.setApplicationMenu(menu)
   //win.setMenu(menu) // for linux and windows only (necessary?)
 
   // and load the index.html of the app.
